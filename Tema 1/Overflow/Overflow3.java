f11d67fe26df0353c3b9daffae7baf11eb73c499
Alan Orgaz Villegas <147757830+AlanOrgazVillegas@users.noreply.github.com>
2024-05-26 13:38:07 -0600

    Commit de Overflow

---
+++ Tema 1/Overflow/Overflow3.java
@@ -0,0 +1,10 @@
+public class Overflow3 {
+    public static void main(String[] args) {
+        // Ejemplo de desbordamiento en una operación aritmética con enteros grandes
+        long a = Long.MAX_VALUE; // Valor máximo para un long
+        long suma = a + 1; // Intentamos sumar 1 al valor máximo de un long
+
+        // Imprimimos el resultado
+        System.out.println("Resultado de la suma: " + suma); // Se produce un desbordamiento
+    }
+}
---
+++ Tema 1/Overflow/Overflow5.java
@@ -0,0 +1,11 @@
+public class Overflow5 {
+    public static void main(String[] args) {
+        // Ejemplo de desbordamiento en una operación de multiplicación con enteros
+        int a = 1000000; // Número grande
+        int b = 1000000; // Número grande
+        long producto = (long) a * b; // Multiplicación de dos enteros grandes
+
+        // Imprimimos el resultado
+        System.out.println("Resultado de la multiplicación: " + producto); // Se produce un desbordamiento
+    }
+}
