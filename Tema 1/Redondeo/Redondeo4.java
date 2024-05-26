adde23a1ee2511402bf0445ee885ad11cc7e659b
Alan Orgaz Villegas <147757830+AlanOrgazVillegas@users.noreply.github.com>
2024-05-26 13:29:47 -0600

    Commit de redondeo

---
+++ Tema 1/Redondeo/Redondeo4.java
@@ -0,0 +1,10 @@
+public class Redondeo4 {
+    public static void main(String[] args) {
+        // Ejemplo de error de redondeo en suma con números muy pequeños
+        double num1 = 1.0e-16; // Número muy pequeño
+        double num2 = 1.0e-16; // Otro número muy pequeño
+        double suma = num1 + num2;
+
+        System.out.println("Suma: " + suma); // Imprime 0.0 en lugar de un valor muy pequeño
+    }
+}
